1c8c3a0d8dbf39d47cbc8f4cf4be17e7e63f4661
imaduddinaf <pentol231094@gmail.com>
2015-04-09 14:12:51 +0700

    Base game (platform, box, spike, runner)

---
+++ TVRunner/TVRunner/Assets/TVRunner/Runner/Runner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Runner : MonoBehaviour {
+
+	public float velocity;
+	public float jumpHeight;
+
+	private bool touchingPlatform;
+	void Die() {
+
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		transform.Translate(velocity * Time.deltaTime, 0f, 0f);
+	}
+
+	void FixedUpdate(){
+		if (touchingPlatform && Input.GetButtonDown ("Jump")) {
+			Debug.Log("jumped");
+			//GetComponent<Rigidbody>().velocity = new Vector3(0.0f, jumpHeight, 0.0f);
+			GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, jumpHeight, 0.0f));
+		}
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.name == "Spike" || col.gameObject.name == "Box Front") {
+			Debug.Log("Trigger Die");
+			Die ();
+		}
+	}
+	
+	void OnCollisionEnter (Collision col) {
+
+		touchingPlatform = true;
+	}
+	
+	void OnCollisionExit () {
+		touchingPlatform = false;
+	}
+}
