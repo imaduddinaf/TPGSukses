7e713816943a48d9ca3a977a3a7183cc0ae42da7
imaduddinaf <pentol231094@gmail.com>
2015-04-10 22:19:41 +0700

    change to 2d assets

---
+++ TVRunner/TVRunner/Assets/TVRunner/Runner/3D/Runner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Runner : MonoBehaviour {
+
+	public float velocity;
+	public float jumpHeight;
+
+	private bool touchingPlatform;
+	void Die() {
+
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		transform.Translate(velocity * Time.deltaTime, 0f, 0f);
+	}
+
+	void FixedUpdate(){
+		if (touchingPlatform && (Input.GetButtonDown ("Jump") || Input.touchCount > 0)) {
+			Debug.Log("jumped");
+			GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, jumpHeight, 0.0f));
+			//GetComponent<Rigidbody2D>().AddForce(new Vector3(0.0f, jumpHeight, 0.0f));
+		}
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.name == "Spike" || col.gameObject.name == "Box Front") {
+			Debug.Log("Trigger Die");
+			Die ();
+		}
+		if (col.gameObject.name == "Portal") {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+	
+	void OnCollisionEnter (Collision col) {
+
+		touchingPlatform = true;
+	}
+	
+	void OnCollisionExit () {
+		touchingPlatform = false;
+	}
+}
---
+++ TVRunner/TVRunner/Assets/TVRunner/Runner/Runner2D.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Runner2D : MonoBehaviour {
+
+	public float velocity;
+	public float jumpHeight;
+
+	private bool touchingPlatform;
+	void Die() {
+
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		transform.Translate(velocity * Time.deltaTime, 0f, 0f);
+		if (GetComponent<Transform>().position.x > 200) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+	void FixedUpdate(){
+		if (touchingPlatform && (Input.GetButtonDown ("Jump") || Input.touchCount > 0)) {
+			Debug.Log("jumped");
+			//GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, jumpHeight, 0.0f));
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, jumpHeight));
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.gameObject.name == "Spike" || col.gameObject.name == "Box Front") {
+			Debug.Log("Trigger Die");
+			Die ();
+		}
+		if (col.gameObject.name == "Portal") {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+	
+	void OnCollisionEnter2D (Collision2D col) {
+		touchingPlatform = true;
+	}
+	
+	void OnCollisionExit2D () {
+		touchingPlatform = false;
+	}
+}
