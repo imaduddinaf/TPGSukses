58db504ed78c7b46a7d6e04913a0c1b4b517da2a
Fajar Setiawan <flashzeth@gmail.com>
2015-04-13 18:30:54 +0700

    add item (true and false), particle, battery bar

---
+++ TVRunner/TVRunner/Assets/TVRunner/Item/item.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class item : MonoBehaviour {
+
+	// Use this for initialization
+	public bool type;
+	private int energyValue;
+	private player playerr;
+	//public GUIText Tester;
+	public TextMesh displaytext;
+	//private int vel;
+	private int inoperasi;
+	public int bilangan1;
+	public int bilangan2;
+	public Transform itemget;
+
+	void Start () {
+		GameObject playerrObject = GameObject.Find ("Runner 2D");
+		if (playerrObject != null){
+			playerr = playerrObject.GetComponent <player>();
+		}
+		if (playerr == null){
+			Debug.Log ("Cannot find 'player' script");
+		}
+		//vel = playerr.playervalue;
+		//Tester.text = "random2: " + vel;
+		Invoke("getovel",0f);
+		//getovel ();
+		//boo.text = "test";
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		//vel = playerr.playervalue;
+		//Tester.text = "random2: " + vel;
+	}
+	void OnCollisionEnter2D(Collision2D coll){
+		if(coll.gameObject.name == "Runner 2D")
+		{
+			Transform efek = Instantiate(itemget,transform.position,transform.rotation) as Transform;
+			Destroy(efek.gameObject,1f);
+			Destroy(gameObject);
+			playerr.AddEnergy(energyValue);
+		}
+	}
+
+	void getovel(){
+		//vel = playerr.playervalue;
+		if (type == true) {
+			energyValue = 5;
+			inoperasi = Random.Range (1, 5);
+			if (inoperasi == 1) {
+				bilangan1 = Random.Range (1, playerr.playervalue);
+				bilangan2 = playerr.playervalue - bilangan1;
+				displaytext.text = bilangan1 + " + " + bilangan2;
+			} else if (inoperasi == 2) {
+				bilangan1 = Random.Range (playerr.playervalue + 1, 99);
+				bilangan2 = bilangan1 - playerr.playervalue;
+				displaytext.text = bilangan1 + " - " + bilangan2;
+			} else if (inoperasi == 3) {
+				bilangan2 = Random.Range (1, 10);
+				bilangan1 = playerr.playervalue * bilangan2;
+				displaytext.text = bilangan1 + " / " + bilangan2;
+			} else if (inoperasi == 4) {
+				while (true) {
+					bilangan1 = Random.Range (1, playerr.playervalue + 1);
+					if (playerr.playervalue % bilangan1 == 0) {
+						bilangan2 = playerr.playervalue / bilangan1;
+						break;
+					}
+				}
+				displaytext.text = bilangan1 + " * " + bilangan2;
+			}
+		} 
+		else {
+			energyValue = -3;
+			inoperasi = Random.Range (1, 5);
+			bilangan1 = Random.Range(1,100);
+			bilangan2 = Random.Range(1, 100);
+			if (inoperasi == 1) {
+				if((bilangan1 + bilangan2) == playerr.playervalue){
+					while(true){
+						bilangan1 = Random.Range(1,100);
+						bilangan2 = Random.Range(1, 100);
+						if(bilangan1 + bilangan2 != playerr.playervalue) break;
+					}
+				}
+				displaytext.text = bilangan1 + " + " + bilangan2;
+			} else if (inoperasi == 2) {
+				if((bilangan1 - bilangan2) == playerr.playervalue){
+					while(true){
+						bilangan1 = Random.Range(1,100);
+						bilangan2 = Random.Range(1, 100);
+						if(bilangan1 - bilangan2 != playerr.playervalue) break;
+					}
+				}
+				displaytext.text = bilangan1 + " - " + bilangan2;
+			} else if (inoperasi == 3) {
+				if((bilangan1 / bilangan2) == playerr.playervalue){
+					while(true){
+						bilangan1 = Random.Range(1,100);
+						bilangan2 = Random.Range(1, 100);
+						if(bilangan1 / bilangan2 != playerr.playervalue) break;
+					}
+				}
+				displaytext.text = bilangan1 + " / " + bilangan2;
+			} else if (inoperasi == 4) {
+				if((bilangan1 * bilangan2) == playerr.playervalue){
+					while(true){
+						bilangan1 = Random.Range(1,100);
+						bilangan2 = Random.Range(1, 100);
+						if(bilangan1 * bilangan2 != playerr.playervalue) break;
+					}
+				}
+				displaytext.text = bilangan1 + " * " + bilangan2;
+			}
+		}
+	}
+}
---
+++ TVRunner/TVRunner/Assets/TVRunner/Runner/player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+
+	// Use this for initialization
+	public float movespeed;
+	//public GUIText scoreText;
+	public TextMesh Test;
+	public int energy;
+	public int playervalue;
+	void Start () {
+		energy = 20;
+		InvokeRepeating("DecreaseBattery", 1f, 1f);
+		UpdateBattery ();
+		playervalue = Random.Range (1, 10);
+		Test.text = "" + playervalue;
+	}
+
+	void Update () {
+		UpdateBattery ();
+		if (energy == 0){
+			Application.LoadLevel(0);
+		}
+	}
+
+	public void AddEnergy (int newEnergyValue){
+		energy += newEnergyValue;
+		UpdateBattery ();
+	}
+
+	void UpdateBattery (){
+		//scoreText.text = "Energy: " + energy;
+	}
+	void DecreaseBattery(){
+		energy--;
+	}
+}
