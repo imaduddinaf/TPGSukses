4e7b1b433853ba205230c57825f15f792bd58137
imaduddinaf <pentol231094@gmail.com>
2015-04-16 07:10:05 +0700

    aduh kok nambah bug energy ga bisa nambah

---
+++ TVRunner/TVRunner/Assets/TVRunner/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour {
+	public Transform prefabGUI;
+	public Transform prefabBackground;
+	private Queue<Transform> queue;
+	// Use this for initialization
+	void Start () {
+		queue = new Queue<Transform> (2);
+		queue.Enqueue ((Transform)Instantiate (prefabGUI));
+		queue.Enqueue ((Transform)Instantiate (prefabBackground));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
