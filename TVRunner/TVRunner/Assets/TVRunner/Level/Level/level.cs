940b8e016f0f3404c795b980525ed9f574ddb798
Fajar Setiawan <flashzeth@gmail.com>
2015-05-04 16:15:37 +0700

    add level handler bonus energy per level energy run out before the portal

--- TVRunner/TVRunner/Assets/TVRunner/Level/Level/level.cs
+++ TVRunner/TVRunner/Assets/TVRunner/Level/Level/level.cs
@@ -10,6 +10,7 @@ public class level : MonoBehaviour {
 	public int batasMax;
 	public int batasMin;
 	public int energyDrain;
+	public int bonusEnergy;
 	private string currentLevel;
 
 	// Use this for initialization
@@ -26,15 +27,17 @@ public class level : MonoBehaviour {
 
 	void LevelHandler(){
 		if (currentLevel == "Tutorial") {
+			bonusEnergy = 0;
 			energyDrain = 1;
 			batasPlayer = 4;
-			batasOperasi = 5;
+			batasOperasi = 3;
 			nilaiBenar = 10;
 			nilaiSalah = -3;
 			batasMax = 10;
 			batasMin = 0;
 		}
 		else if (currentLevel == "Level 1") {
+			bonusEnergy = 0;
 			energyDrain = 1;
 			batasPlayer = 4;
 			batasOperasi = 2;
@@ -44,6 +47,117 @@ public class level : MonoBehaviour {
 			batasMin = 0;
 		}
 		else if(currentLevel == "Level 2"){
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 4;
+			batasOperasi = 2;
+			nilaiBenar = 10;
+			nilaiSalah = -3;
+			batasMax = 10;
+			batasMin = 0;
+		}
+		else if (currentLevel == "Level 3") {
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 6;
+			batasOperasi = 2;
+			nilaiBenar = 8;
+			nilaiSalah = -4;
+			batasMax = 15;
+			batasMin = 0;
+		}
+		else if(currentLevel == "Level 4"){
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 4;
+			batasOperasi = 2;
+			nilaiBenar = 10;
+			nilaiSalah = -3;
+			batasMax = 10;
+			batasMin = 0;
+		}
+		else if (currentLevel == "Level 5") {
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 4;
+			batasOperasi = 2;
+			nilaiBenar = 10;
+			nilaiSalah = -3;
+			batasMax = 10;
+			batasMin = 0;
+		}
+		else if(currentLevel == "Level 6"){
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 4;
+			batasOperasi = 2;
+			nilaiBenar = 10;
+			nilaiSalah = -3;
+			batasMax = 10;
+			batasMin = 0;
+		}
+		else if (currentLevel == "Level 7") {
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 4;
+			batasOperasi = 2;
+			nilaiBenar = 10;
+			nilaiSalah = -3;
+			batasMax = 10;
+			batasMin = 0;
+		}
+		else if(currentLevel == "Level 8"){
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 4;
+			batasOperasi = 2;
+			nilaiBenar = 10;
+			nilaiSalah = -3;
+			batasMax = 10;
+			batasMin = 0;
+		}
+		else if (currentLevel == "Level 9") {
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 4;
+			batasOperasi = 2;
+			nilaiBenar = 10;
+			nilaiSalah = -3;
+			batasMax = 10;
+			batasMin = 0;
+		}
+		else if(currentLevel == "Level 10"){
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 4;
+			batasOperasi = 2;
+			nilaiBenar = 10;
+			nilaiSalah = -3;
+			batasMax = 10;
+			batasMin = 0;
+		}
+		else if (currentLevel == "Level 11") {
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 4;
+			batasOperasi = 2;
+			nilaiBenar = 10;
+			nilaiSalah = -3;
+			batasMax = 10;
+			batasMin = 0;
+		}
+		else if(currentLevel == "Level 12"){
+			bonusEnergy = 0;
+			energyDrain = 1;
+			batasPlayer = 4;
+			batasOperasi = 2;
+			nilaiBenar = 10;
+			nilaiSalah = -3;
+			batasMax = 10;
+			batasMin = 0;
+		}
+		else if(currentLevel == "Level 13"){
+			bonusEnergy = 0;
 			energyDrain = 1;
 			batasPlayer = 4;
 			batasOperasi = 2;
