622f2a48799c1003ea57883ba36de3a9f47141dd
imaduddinaf <pentol231094@gmail.com>
2015-05-18 14:40:51 +0700

    lock aspect ratio ke 16:9

---
+++ TVRunner/TVRunner/Assets/TVRunner/Menu/Ingame/IngameMenu.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngameMenu : MonoBehaviour {
+	private bool pauseEnabled;
+	private bool gameOver;
+	private bool congrats;
+	//menu
+	private float menuWidth;
+	private float menuHeight;
+	float screenWidth;
+	float screenHeight;
+	float distanceBetweenMenus;
+	//behaviour
+
+	public void Pause(){
+		pauseEnabled = true;
+		Time.timeScale = 0;
+	}
+
+	public void Resume(){
+		pauseEnabled = false;
+		Time.timeScale = 1;
+	}
+
+	public void GameOver(){
+		gameOver = true;
+		Time.timeScale = 0;
+	}
+
+	public void Congrats(){
+		congrats = true;
+		Time.timeScale = 0;
+	}
+
+	public void Quit(){
+		MasterData.currentLevel = 0;
+		Application.LoadLevel("Main Menu");
+	}
+
+	public void NextLevel() {
+		MasterData.currentLevel += 1;
+		Debug.Log (MasterData.currentLevel + ".." + MasterData.levelMax);
+		if (MasterData.currentLevel > MasterData.levelMax) {
+			Application.LoadLevel ("World Map");
+		} else {
+			Application.LoadLevel ("Level " + MasterData.currentLevel);
+		}
+	}
+
+	public void Restart(){		
+		Resume();
+		Application.LoadLevel(Application.loadedLevel);
+	}
+	// Use this for initialization
+	void Start () {
+		menuWidth = Screen.width * 0.5f;
+		menuHeight = Screen.height * 0.7f;
+		pauseEnabled = false;
+		gameOver = false;
+		congrats = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Pause ();
+		/*if (!pauseEnabled && Input.GetKeyDown (KeyCode.Escape)) {
+			//Time.timeScale = 0;
+			//Application.Quit();
+			Pause();
+		}*/
+		if (Input.GetKeyDown (KeyCode.Backspace) || Input.GetKeyDown (KeyCode.Escape)) {
+			if(pauseEnabled && !gameOver && !congrats) {
+				Resume();
+			}
+			else if(!pauseEnabled && !gameOver && !congrats) {
+				Pause();
+			}
+		}
+		/*if (Input.GetKeyDown ("space")) {
+			Debug.Log("space pressed");
+		}*/
+	}
+
+	void OnGUI(){
+		screenWidth = Screen.width;
+		screenHeight = Screen.height;
+		distanceBetweenMenus = menuHeight * 0.05f;
+		//menu pause
+		if (pauseEnabled) {
+			GUI.Box (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f), (screenHeight * 0.5f) - (screenHeight * 0.35f), menuWidth, menuHeight), "menu box");
+			if (GUI.Button (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f ) + (menuWidth * 0.1f), (screenHeight * 0.5f) - (screenHeight * 0.35f) + (menuHeight * 0.15f), menuWidth * 0.8f, menuHeight * 0.2f), "Resume")) {
+				Debug.Log ("Resume");
+				Resume();
+			}
+			if (GUI.Button (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f ) + (menuWidth * 0.1f), (screenHeight * 0.5f) - (screenHeight * 0.35f) + (menuHeight * 0.15f) + (distanceBetweenMenus + (menuHeight * 0.2f)), menuWidth * 0.8f, menuHeight * 0.2f), "Restart")) {
+				Debug.Log ("Restart");
+				Restart();
+			}
+			if (GUI.Button (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f ) + (menuWidth * 0.1f), (screenHeight * 0.5f) - (screenHeight * 0.35f) + (menuHeight * 0.15f) + ((distanceBetweenMenus * 2) + (menuHeight * 0.2f * 2)), menuWidth * 0.8f, menuHeight * 0.2f), "Quit")) {
+				Debug.Log ("Quit");
+				Quit ();
+			}
+		}
+		//menu gameover
+		if (gameOver) {
+			GUI.Box (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f), (screenHeight * 0.5f) - (screenHeight * 0.35f), menuWidth, menuHeight), "game over");
+			if (GUI.Button (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f ) + (menuWidth * 0.1f), (screenHeight * 0.5f) - (screenHeight * 0.35f) + (menuHeight * 0.15f), menuWidth * 0.8f, menuHeight * 0.2f), "Restart")) {
+				Debug.Log ("Restart");
+				Restart();
+			}
+			if (GUI.Button (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f ) + (menuWidth * 0.1f), (screenHeight * 0.5f) - (screenHeight * 0.35f) + (menuHeight * 0.15f) + (distanceBetweenMenus + (menuHeight * 0.2f)), menuWidth * 0.8f, menuHeight * 0.2f), "Quit")) {
+				Debug.Log ("Quit");
+				Quit ();
+			}
+		}
+		//menu win-congrats
+		if (congrats) {
+			GUI.Box (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f), (screenHeight * 0.5f) - (screenHeight * 0.35f), menuWidth, menuHeight), "congratulations!!");
+			if (GUI.Button (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f ) + (menuWidth * 0.1f), (screenHeight * 0.5f) - (screenHeight * 0.35f) + (menuHeight * 0.15f), menuWidth * 0.8f, menuHeight * 0.2f), "Restart")) {
+				Debug.Log ("Restart");
+				Restart();
+			}
+			if (GUI.Button (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f ) + (menuWidth * 0.1f), (screenHeight * 0.5f) - (screenHeight * 0.35f) + (menuHeight * 0.15f) + (distanceBetweenMenus + (menuHeight * 0.2f)), menuWidth * 0.8f, menuHeight * 0.2f), "Next Level")) {
+				Debug.Log ("Next Level");
+				NextLevel();
+			}
+			if (GUI.Button (new Rect ((screenWidth * 0.5f) - (screenWidth * 0.25f ) + (menuWidth * 0.1f), (screenHeight * 0.5f) - (screenHeight * 0.35f) + (menuHeight * 0.15f) + ((distanceBetweenMenus * 2) + (menuHeight * 0.2f * 2)), menuWidth * 0.8f, menuHeight * 0.2f), "Quit")) {
+				Debug.Log ("Quit");
+				Quit ();
+			}
+		}
+	}
+}
+		
---
+++ TVRunner/TVRunner/Assets/TVRunner/Menu/Main Menu/MasterData.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using SimpleJSON;
+using UnityEngine.UI;
+
+public class MasterData : MonoBehaviour {
+	private string data;
+	public Text version;
+	public static int currentLevel = 0;
+	public static float volume;
+	public static string gameVersion;
+	public static int levelMax;
+	void LoadFromFile(){
+		//json
+		SimpleJSON.JSONNode node = SimpleJSON.JSONNode.Parse(File.ReadAllText(Application.dataPath +"/data.json"));
+		gameVersion = node ["version"];
+		volume = node ["volume"].AsFloat;
+		levelMax = node ["levelmax"].AsInt;
+		//Debug.Log(gameVersion + "\n" + volume.ToString() + "\n" + levelMax.ToString());
+		//version.text = volume.ToString();
+	}
+	public static void WriteToFile(){
+		//json
+		SimpleJSON.JSONNode node = SimpleJSON.JSONNode.Parse(File.ReadAllText(Application.dataPath +"/data.json"));
+		node ["version"] = gameVersion;
+		node ["volume"].AsFloat = volume;
+		node ["levelmax"].AsInt = levelMax;
+		File.WriteAllText(Application.persistentDataPath + "/data.json", node.ToString());
+
+		//test
+		/*if (System.IO.File.Exists("myfile.txt"))
+		{
+			//do stuff
+		}*/
+	}
+	// Use this for initialization
+	void Start () {
+		//DontDestroyOnLoad (transform.gameObject);
+		if (System.IO.File.Exists ("data.json")) {
+			Debug.Log("exist");
+			string tmp = 
+				"{\n" +
+					"\t" + "\"version\" : \"v 1.0\" " + ", \n" + 
+					"\t" + "\"volume\" : \"1.0\" " + ", \n" + 
+					"\t" + "\"levelmax\" : \"4\" " + ", \n" + 
+					"}";
+			File.WriteAllText (Application.dataPath + "/data.json", tmp);
+			LoadFromFile ();
+		} else {
+			LoadFromFile ();
+		}
+		levelMax = 4;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (levelMax);
+		version.text = gameVersion;
+		//Debug.Log (volume.ToString ());
+		/*if(Input.GetKeyDown("space")){
+			//volume += 0.1f;
+			WriteToFile();
+		}*/
+		if (currentLevel > levelMax) {
+			levelMax = currentLevel;
+			WriteToFile();
+		}
+		//Debug.Log (MasterData.GameVersion);
+	}
+
+	//setter getter
+	public static string GameVersion
+	{
+		get { return gameVersion; }
+		set { gameVersion = value; }
+	}
+}
