288ec25979d8f30cb1d538fbc17a3c4b89acbd03
aghagha <amrikudou@gmail.com>
2015-08-14 16:20:40 +0700

    Portal lebih besar

--- TVRunner/TVRunner/Assets/TVRunner/Level/Level/level.cs
+++ TVRunner/TVRunner/Assets/TVRunner/Level/Level/level.cs
@@ -46,7 +46,7 @@ public class level : MonoBehaviour {
 		}
 		else if (currentLevel == "Level 1") {
 			bonusEnergy = 0;
-			energyDrain = 1;
+			energyDrain = 0;
 			batasPlayer = 4;
 			batasOperasi = 2;
 			nilaiBenar = 10;
